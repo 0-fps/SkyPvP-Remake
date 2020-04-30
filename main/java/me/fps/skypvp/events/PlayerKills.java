91557e9acd21fa3e40d988b06b9025494b51ddda
0-fps <64110582+0-fps@users.noreply.github.com>
2020-04-30 18:37:22 -0300

    Update PlayerKills.java

--- main/java/me/fps/skypvp/events/PlayerKills.java
+++ main/java/me/fps/skypvp/events/PlayerKills.java
@@ -5,12 +5,16 @@ import me.clip.placeholderapi.PlaceholderAPI;
 import me.fps.skypvp.skypvp;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import org.bukkit.event.entity.PlayerDeathEvent;
+
 
 public class PlayerKills implements Listener {
     private skypvp plugin;
@@ -21,113 +25,74 @@ public class PlayerKills implements Listener {
 
     @EventHandler
     public void KillPlayer(PlayerDeathEvent event) {
-        Player Killer = event.getEntity().getKiller();
+        FileConfiguration players = plugin.getPlayers();
+        Player player = event.getEntity();
         Economy econ = plugin.getEconomy();
         FileConfiguration config = plugin.getConfig();
-        FileConfiguration players = plugin.getPlayers();
-        Player Victim = event.getEntity();
-        String victimworld = Victim.getWorld().getName();
+        Boolean cancelled = config.getBoolean("Config.Cancel-Drops");
+        int deaths = Integer.valueOf(players.getString("Players." + player.getUniqueId() + ".deaths"));
+        players.set("Players." + player.getUniqueId() + ".deaths", deaths + 1);
+        String victimworld = player.getWorld().getName();
         String configworld = config.getString("Config.Spawn.world");
-        String prestige = players.getString("Players."+Killer.getUniqueId()+".prestige");
-        players.set("Players." + Killer.getUniqueId() + ".name", Killer.getName());
+        players.set("Players." + player.getUniqueId() + ".name", player.getName());
         if (configworld.toLowerCase().equals(victimworld.toLowerCase())) {
-            if (players.contains("Players." + Killer.getUniqueId() + ".kills") && players.contains("Players." + Killer.getUniqueId() + ".points")){
-                if (Killer.getPlayer() == null){
-                    String nokillerformat = config.getString("Config.No-Killer-Death-Message");
-                    String vaultvictim = "%vault_prefix%";
-                    vaultvictim = PlaceholderAPI.setPlaceholders(Killer.getPlayer(), vaultvictim);
-                    nokillerformat = ChatColor.translateAlternateColorCodes('&', nokillerformat);
-                    nokillerformat = nokillerformat.replace("%victim%", Victim.getDisplayName() + "").replace("%vault_prefix%", vaultvictim+"");
-                    event.setDeathMessage(nokillerformat);
-                }else{
+            if (players.contains("Players." + player.getUniqueId() + ".kills") && players.contains("Players." + player.getUniqueId() + ".points")) {
+                if (player.getKiller() instanceof Player) {
+                    Player killer = event.getEntity().getKiller();
+                    int kills = Integer.valueOf(players.getString("Players." + killer.getUniqueId() + ".kills"));
+                    String prestige = players.getString("Players."+killer.getUniqueId()+".prestige");
+                    int points = Integer.valueOf(players.getString("Players." + killer.getUniqueId() + ".points"));
+                    players.set("Players." + killer.getUniqueId() + ".points", points + 2);
+                    players.set("Players." + killer.getUniqueId() + ".kills", kills + 1);
+                    plugin.savePlayers();
+                    ConfigurationSection section = config.getConfigurationSection("Config.Prestiges");
+                    if(section != null) {
+                        for (String prestiges : config.getConfigurationSection("Config.Prestiges").getKeys(false)){
+                            int pointstoget = config.getInt("Config.Prestiges." + prestiges + ".Points");
+                            if(points == pointstoget){
+                                players.set("Players."+killer.getUniqueId()+".prestige", prestiges);
+                            }
+                        }
+                    }
+
+                    plugin.savePlayers();
+                    Double ganance = config.getDouble("Config.KillMoney");
+                    String moneymessage = config.getString("Config.KillMoney-Message");
+                    moneymessage = ChatColor.translateAlternateColorCodes('&', moneymessage);
+                    moneymessage = moneymessage.replace("%victim%", player.getDisplayName() + "").replace("%money%", ganance + "");
+                    econ.depositPlayer(killer, ganance);
+                    killer.sendMessage(ChatColor.translateAlternateColorCodes('&', moneymessage));
                     String vaultprefix = "%vault_prefix%";
-                    vaultprefix = PlaceholderAPI.setPlaceholders(Killer.getPlayer(), vaultprefix);
+                    vaultprefix = PlaceholderAPI.setPlaceholders(killer.getPlayer(), vaultprefix);
                     String format = config.getString("Config.Death-Message");
                     format = ChatColor.translateAlternateColorCodes('&', format);
-                    format = format.replace("%killer%", Killer.getDisplayName() + "").replace("%victim%", Victim.getDisplayName() + "").replace("%prestige%", prestige + "").replace("%vault_prefix%", vaultprefix + "");
+                    format = format.replace("%killer%", killer.getDisplayName() + "").replace("%victim%", player.getDisplayName() + "").replace("%prestige%", prestige + "").replace("%vault_prefix%", vaultprefix + "");
                     event.setDeathMessage(format);
-                }
-
-                Double ganance = config.getDouble("Config.KillMoney");
-                String moneymessage = config.getString("Config.KillMoney-Message");
-                moneymessage = ChatColor.translateAlternateColorCodes('&', moneymessage);
-                moneymessage  = moneymessage.replace("%victim%", Victim.getDisplayName() + "").replace("%money%", ganance+"");
-                econ.depositPlayer(Killer, ganance);
-                Killer.sendMessage(ChatColor.translateAlternateColorCodes('&', moneymessage));
-                int points = Integer.valueOf(players.getString("Players." + Killer.getUniqueId() + ".points"));
-                players.set("Players." + Killer.getUniqueId() + ".points", points + 2);
-                int kills = Integer.valueOf(players.getString("Players." + Killer.getUniqueId() + ".kills"));
-                players.set("Players." + Killer.getUniqueId() + ".kills", kills + 1);
-                if (points <= 9) {
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§8No Prestige");
+                    player.spigot().respawn();
+                    double x = Double.valueOf(config.getString("Config.Spawn.x"));
+                    double y = Double.valueOf(config.getString("Config.Spawn.y"));
+                    double z = Double.valueOf(config.getString("Config.Spawn.z"));
+                    float yaw = Float.valueOf(config.getString("Config.Spawn.yaw"));
+                    float pitch = Float.valueOf(config.getString("Config.Spawn.pitch"));
+                    World world = plugin.getServer().getWorld(config.getString("Config.Spawn.world"));
+                    Location l = new Location(world, x, y, z, yaw, pitch);
+                    player.teleport(l);
+                    plugin.savePlayers();
+                    if (cancelled != false){
+                        event.getDrops().clear();
+                    }
                     plugin.savePlayers();
                     return;
-                }else if(points <= 10){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§8Coal IV");
-                    plugin.savePlayers();
-                }else if(points <= 15){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§8Coal III");
-                    plugin.savePlayers();
-                }else if(points <= 20){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§8Coal II");
-                    plugin.savePlayers();
-                }else if(points <= 25){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§8Coal I");
-                    plugin.savePlayers();
-                }else if(points <= 35){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§7Iron V");
-                    plugin.savePlayers();
-                }else if(points <= 40){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§7Iron IV");
-                    plugin.savePlayers();
-                }else if(points <= 45){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§7Iron III");
-                    plugin.savePlayers();
-                }else if(points <= 50){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§7Iron II");
-                    plugin.savePlayers();
-                }else if(points <= 55){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§7Iron I");
-                    plugin.savePlayers();
-                }else if(points <= 65){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§6Gold III");
-                    plugin.savePlayers();
-                }else if(points <= 70){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§6Gold II");
-                    plugin.savePlayers();
-                }else if(points <= 75){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§6Gold I");
-                    plugin.savePlayers();
-                }else if(points <= 85){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§aEmerald IV");
-                    plugin.savePlayers();
-                }else if(points <= 90){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§aEmerald III");
-                    plugin.savePlayers();
-                }else if(points <= 95){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§aEmerald II");
-                    plugin.savePlayers();
-                }else if(points <= 100){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§aEmerald I");
-                    plugin.savePlayers();
-                }else if(points <= 110){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§bDiamond IV");
-                    plugin.savePlayers();
-                }else if(points <= 130){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§bDiamond III");
-                    plugin.savePlayers();
-                }else if(points <= 160){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§bDiamond II");
-                    plugin.savePlayers();
-                }else if(points <= 190){
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§bDiamond I");
-                    plugin.savePlayers();
-                }else{
-                    players.set("Players." + Killer.getUniqueId() + ".prestige", "§bDiamond I");
-                    plugin.savePlayers();
+
                 }
+                plugin.savePlayers();
+                return;
+
 
             }
+            plugin.savePlayers();
+            return;
+
 
         }
 
